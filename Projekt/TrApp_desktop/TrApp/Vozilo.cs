a618fee00e0bf07567480f291cadbe127ea188d5
mathabaws <matha.baws@gmail.com>
2014-04-18 11:57:17 +0200

    Dijagram klasa v1
    Ovo je početna verzija dijagrama klasa u kojoj su napravljene osnovne
    klase za vozila, servis, tehnički pregled i vozače

---
+++ Projekt/TrApp_desktop/TrApp/Vozilo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrApp
+{
+    public class Vozilo
+    {
+        public int idVozila = 0;
+        public string naziv;
+        public int godProizvodnje;
+        public string registracija;
+        public string datRegistracije;
+        public int pocStanjeKm;
+        public int nosivost;
+        public int servisniIntervalKm;
+        
+
+
+        /// <summary>
+        ///     Konstruktor unutar kojeg se postavljaju unesene vrijednosti vozila
+        /// </summary>
+        /// <param name="naziv">Naziv vozila</param>
+        /// <param name="godProizvodnje">Godina proizvodnje vozila</param>
+        /// <param name="registracija">Registarska oznaka vozila </param>
+        /// <param name="datRegistracija">Datum isteka registacije za vozilo</param>
+        /// <param name="pocStanjeKm">Početno stanje kilometara vozila</param>
+        /// <param name="nosivost">Nosivost vozila u kilogramima</param>
+        /// <param name="servisniIntervalKm">Interval nakon kojeg vozilo mora ici na servis(u kilometrima)</param>
+        public Vozilo(string naziv, int godProizvodnje, string registracija, string datRegistracije, int pocStanjeKm,
+            int nosivost, int servisniIntervalKm ) 
+        {
+            this.naziv = naziv;
+            this.godProizvodnje = godProizvodnje;
+            this.registracija = registracija;
+            this.datRegistracije = datRegistracije;
+            this.pocStanjeKm = pocStanjeKm;
+            this.nosivost = nosivost;
+            this.servisniIntervalKm = servisniIntervalKm;
+            
+        }//constr
+
+        public Vozilo()
+        { 
+            //to do nothing!
+        }
+
+        public void unosVozila()
+        {
+            //Poziv metode za spajanje na bazu i unos podataka!!!11
+        }
+        public void brisanjeVozila()
+        {
+            //to do
+        }
+
+        public void izmjenaVozila()
+        {
+            //izmjenjuje osnovne podatke vozila npr. izmjena registracije
+        }
+
+        public virtual void  ispisVozila()
+        {
+            //Nešto što dohvaća podatke iz baze i vraća ih nekom dijelu forme
+        }
+
+        
+        
+    }//class Vozilo
+}
---
+++ Projekt/TrApp_desktop/TrApp/Zaposlenik.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrApp
+{
+    public class Zaposlenik
+    {
+        public int idZaposlenika;
+        public string ime;
+        public string prezime;
+        public string oib;
+        public string datZaposlenja;
+        public string iban;
+        public string brTel;
+        public string adresaStan;
+        public string datRodjenja;
+
+        /// <summary>
+        ///         //To do!!111
+        /// </summary>
+        /// <param name="ime"></param>
+        /// <param name="prezime"></param>
+        /// <param name="oib"></param>
+        /// <param name="datZaposlenja"></param>
+        /// <param name="iban"></param>
+        /// <param name="brTel"></param>
+        /// <param name="adresaStan"></param>
+        /// <param name="datRodjenja"></param>
+        public Zaposlenik(string ime, string prezime, string oib, string datZaposlenja,
+            string iban, string brTel, string adresaStan, string datRodjenja)
+        {
+
+            this.ime = ime;
+            this.prezime = prezime;
+            this.oib = oib;
+            this.datZaposlenja = datZaposlenja;
+            this.iban = iban;
+            this.brTel = brTel;
+            this.adresaStan = adresaStan;
+            this.datRodjenja = datRodjenja;
+        }//constr
+
+        public void unosZaposlenika() 
+        {
+            // to do 
+        }
+
+        public void brisanjeZaposlenika()
+        {
+            //to do
+        }
+
+        public void izmjenaZaposlenika()
+        {
+            //to do
+        }
+        public void ispisZaposlenika()
+        {
+            //to do
+        }
+
+    }//class
+}
