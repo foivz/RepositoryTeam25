cdb2dcc08353a8998dcd564c131dfd07cb46f181
Martina Šestak <sestak.martina@gmail.com>
2014-06-19 10:56:48 +0100

    TrApp Praćenje vozila
    Napravljena je windows forma s prikazom početnog markera i rute

---
+++ Projekt/PracenjeVozila/PracenjeVozila/frmPracenje.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace PracenjeVozila
+{
+    partial class frmPracenje
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapControl = new GMap.NET.WindowsForms.GMapControl();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mapControl
+            // 
+            this.mapControl.Bearing = 0F;
+            this.mapControl.CanDragMap = true;
+            this.mapControl.GrayScaleMode = false;
+            this.mapControl.LevelsKeepInMemmory = 5;
+            this.mapControl.Location = new System.Drawing.Point(3, 3);
+            this.mapControl.MarkersEnabled = true;
+            this.mapControl.MaxZoom = 18;
+            this.mapControl.MinZoom = 2;
+            this.mapControl.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.mapControl.Name = "mapControl";
+            this.mapControl.NegativeMode = false;
+            this.mapControl.PolygonsEnabled = true;
+            this.mapControl.RetryLoadTile = 0;
+            this.mapControl.RoutesEnabled = true;
+            this.mapControl.ShowTileGridLines = false;
+            this.mapControl.Size = new System.Drawing.Size(283, 490);
+            this.mapControl.TabIndex = 0;
+            this.mapControl.Zoom = 8D;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(322, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(367, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(322, 244);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(367, 99);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // frmPracenje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.mapControl);
+            this.Name = "frmPracenje";
+            this.Size = new System.Drawing.Size(734, 496);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl mapControl;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
---
+++ Projekt/PracenjeVozila/PracenjeVozila/frmPracenje.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsForms.ToolTips;
+using GMap.NET.MapProviders;
+
+namespace PracenjeVozila
+{
+    public partial class frmPracenje: UserControl
+    {
+        public frmPracenje()
+        {
+            InitializeComponent();
+
+            try
+            {
+                System.Net.IPHostEntry e =
+                     System.Net.Dns.GetHostEntry("www.google.com");
+            }
+            catch
+            {
+                mapControl.Manager.Mode = AccessMode.CacheOnly;
+                MessageBox.Show("Nedostupna internetska veza! Prelazim u Cache način rada...",
+                      "TrApp praćenje vozila", MessageBoxButtons.OK,
+                      MessageBoxIcon.Warning);
+            }
+
+            // add your custom map db provider
+            //GMap.NET.CacheProviders.MySQLPureImageCache ch = new GMap.NET.CacheProviders.MySQLPureImageCache();
+            //ch.ConnectionString = @"server=sql2008;User Id=trolis;Persist Security Info=True;database=gmapnetcache;password=trolis;";
+            //MainMap.Manager.SecondaryCache = ch;
+            mapControl.DragButton = MouseButtons.Left;
+
+            mapControl.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
+            mapControl.Position = new PointLatLng(46.305746000000000000, 16.336606599999982000);
+
+            GMapOverlay markersOverlay = new GMapOverlay(mapControl, "markers");
+            GMapMarkerGoogleGreen marker = new GMapMarkerGoogleGreen(new PointLatLng(46.305746000000000000, 16.336606599999982000));
+            markersOverlay.Markers.Add(marker);
+            mapControl.Overlays.Add(markersOverlay);
+        
+            mapControl.RoutesEnabled = true;
+            PointLatLng start = new PointLatLng(46.305746000000000000, 16.336606599999982000);
+            PointLatLng end = new PointLatLng(45.815011, 15.981919);
+            MapRoute route = GMap.NET.MapProviders.OpenStreetMapProvider.Instance.GetRouteBetweenPoints(start, end, false, false, 15);
+
+
+            GMapRoute r = new GMapRoute(route.Points, "Ruta");
+            r.Stroke.Color = Color.Red;
+            GMapOverlay routesOverlay = new GMapOverlay(mapControl, "routes");
+            routesOverlay.Routes.Add(r);
+            mapControl.Overlays.Add(routesOverlay);
+        }
+    }
+}
