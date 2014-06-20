95ee0eaa54ea77d684c0432c147866e9b7f72897
Martina Šestak <sestak.martina@gmail.com>
2014-06-20 20:57:26 +0100

    Praćenje vozila v2
    Dodana je opcija s popunjavanjem jedne tablice podacima o lokacijama
    vozila na aktivnim rutama.

--- Projekt/PracenjeVozila/PracenjeVozila/frmPracenje.Designer.cs
+++ Projekt/PracenjeVozila/PracenjeVozila/frmPracenje.Designer.cs
@@ -31,6 +31,7 @@
             this.mapControl = new GMap.NET.WindowsForms.GMapControl();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -39,9 +40,10 @@
             // 
             this.mapControl.Bearing = 0F;
             this.mapControl.CanDragMap = true;
+            this.mapControl.Dock = System.Windows.Forms.DockStyle.Left;
             this.mapControl.GrayScaleMode = false;
             this.mapControl.LevelsKeepInMemmory = 5;
-            this.mapControl.Location = new System.Drawing.Point(3, 3);
+            this.mapControl.Location = new System.Drawing.Point(0, 0);
             this.mapControl.MarkersEnabled = true;
             this.mapControl.MaxZoom = 18;
             this.mapControl.MinZoom = 2;
@@ -52,7 +54,7 @@
             this.mapControl.RetryLoadTile = 0;
             this.mapControl.RoutesEnabled = true;
             this.mapControl.ShowTileGridLines = false;
-            this.mapControl.Size = new System.Drawing.Size(283, 490);
+            this.mapControl.Size = new System.Drawing.Size(333, 496);
             this.mapControl.TabIndex = 0;
             this.mapControl.Zoom = 8D;
             // 
@@ -60,33 +62,54 @@
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(322, 41);
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.Location = new System.Drawing.Point(366, 73);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(367, 150);
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.ShowCellErrors = false;
+            this.dataGridView1.ShowCellToolTips = false;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.ShowRowErrors = false;
+            this.dataGridView1.Size = new System.Drawing.Size(326, 150);
             this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // dataGridView2
             // 
             this.dataGridView2.AllowUserToAddRows = false;
             this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(322, 244);
+            this.dataGridView2.Location = new System.Drawing.Point(366, 252);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.ReadOnly = true;
-            this.dataGridView2.Size = new System.Drawing.Size(367, 99);
+            this.dataGridView2.Size = new System.Drawing.Size(326, 116);
             this.dataGridView2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(342, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 3;
             // 
             // frmPracenje
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.mapControl);
             this.Name = "frmPracenje";
             this.Size = new System.Drawing.Size(734, 496);
+            this.Load += new System.EventHandler(this.frmPracenje_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
@@ -98,5 +121,6 @@
         private GMap.NET.WindowsForms.GMapControl mapControl;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Projekt/PracenjeVozila/PracenjeVozila/frmPracenje.cs
+++ Projekt/PracenjeVozila/PracenjeVozila/frmPracenje.cs
@@ -12,18 +12,70 @@ using GMap.NET.WindowsForms;
 using GMap.NET.WindowsForms.Markers;
 using GMap.NET.WindowsForms.ToolTips;
 using GMap.NET.MapProviders;
+using MySql.Data.MySqlClient;
 
 namespace PracenjeVozila
 {
+   
     public partial class frmPracenje: UserControl
     {
+        string connectionString = "server=144.76.19.105;userid=mvukovic2;pwd=L3xxtafb;database=mvukovic2;";
         public frmPracenje()
         {
             InitializeComponent();
+           
+           string query = String.Format("SELECT DISTINCT idptr AS 'ID putnog radnog lista', status AS 'Status rute' from lokacije WHERE idlokacije IN(SELECT MAX(idlokacije) FROM lokacije l1 WHERE l1.idptr=idptr GROUP BY l1.idptr) AND status='t';  ", "bd");
 
+           MySqlConnection conn = new MySqlConnection(connectionString);
+           conn.Open();
+           MySqlCommand command = new MySqlCommand(query, conn);
+           MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+           DataTable data = new DataTable();
+           adapter.Fill(data);
+           
+           dataGridView1.DataSource = data;
+            
+           
+            conn.Close();
+        }
+
+
+        /// <summary>
+        /// Metodom kojom se klikom na polje u prvom datagridu upisuje ID putnog radnog lista u label
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            label1.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            int ptr = int.Parse(label1.Text);
+            string query = String.Format("SELECT DISTINCT * FROM radni_sati WHERE putni_radni_list='ptr';  ", "bd");
+
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            MySqlCommand command = new MySqlCommand(query, conn);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+
+            dataGridView2.DataSource = data;
+
+
+            conn.Close();
+        }
+
+
+
+        /// <summary>
+        /// Metoda koja nakon učitavanja forme priprema kartu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmPracenje_Load(object sender, EventArgs e)
+        {
             try
             {
-                System.Net.IPHostEntry e =
+                System.Net.IPHostEntry ent =
                      System.Net.Dns.GetHostEntry("www.google.com");
             }
             catch
@@ -33,11 +85,6 @@ namespace PracenjeVozila
                       "TrApp praćenje vozila", MessageBoxButtons.OK,
                       MessageBoxIcon.Warning);
             }
-
-            // add your custom map db provider
-            //GMap.NET.CacheProviders.MySQLPureImageCache ch = new GMap.NET.CacheProviders.MySQLPureImageCache();
-            //ch.ConnectionString = @"server=sql2008;User Id=trolis;Persist Security Info=True;database=gmapnetcache;password=trolis;";
-            //MainMap.Manager.SecondaryCache = ch;
             mapControl.DragButton = MouseButtons.Left;
 
             mapControl.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
@@ -48,7 +95,7 @@ namespace PracenjeVozila
             GMapMarkerGoogleGreen marker = new GMapMarkerGoogleGreen(new PointLatLng(46.305746000000000000, 16.336606599999982000));
             markersOverlay.Markers.Add(marker);
             mapControl.Overlays.Add(markersOverlay);
-        
+
             mapControl.RoutesEnabled = true;
             PointLatLng start = new PointLatLng(46.305746000000000000, 16.336606599999982000);
             PointLatLng end = new PointLatLng(45.815011, 15.981919);
@@ -60,6 +107,9 @@ namespace PracenjeVozila
             GMapOverlay routesOverlay = new GMapOverlay(mapControl, "routes");
             routesOverlay.Routes.Add(r);
             mapControl.Overlays.Add(routesOverlay);
+
+            
         }
+      
     }
 }
