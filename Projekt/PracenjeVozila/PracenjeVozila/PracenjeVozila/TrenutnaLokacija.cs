a592ce851b84950b36f19cfbdc8992cbebf3979c
mathabaws <matha.baws@gmail.com>
2014-06-21 02:34:05 +0200

    Praćenje vozila V3 - gotove funkcionalnosti
    Dorađena verzija. Gotove osnovne funkcionalnost: prikaz aktivnih ruta,
    pregled trenutne lokacije, prikaz cjelokupne rute.

---
+++ Projekt/PracenjeVozila/PracenjeVozila/PracenjeVozila/TrenutnaLokacija.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracenjeVozila
+{
+    class TrenutnaLokacija
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+}
